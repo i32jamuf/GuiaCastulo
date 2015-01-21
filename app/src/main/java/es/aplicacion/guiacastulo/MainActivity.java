dd95d45be8db15e4e48c38e1c0583fd8db9736ef
Enmanuel Miron <enmanuel.miron@gmail.com>
2015-01-21 12:44:30 +0100

    Prueba de Push

--- app/src/main/java/es/aplicacion/guiacastulo/MainActivity.java
+++ app/src/main/java/es/aplicacion/guiacastulo/MainActivity.java
@@ -7,11 +7,13 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+    //A ver si hace PUSH
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int i=0;
     }
 
 
