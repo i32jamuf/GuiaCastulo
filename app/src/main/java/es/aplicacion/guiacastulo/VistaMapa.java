bafea7e446695473944adcc3b8c9a40b65bad253
i32jamuf <i32jamuf@hotmail.com>
2015-01-21 12:54:33 +0100

    flag

--- app/src/main/java/es/aplicacion/guiacastulo/VistaMapa.java
+++ app/src/main/java/es/aplicacion/guiacastulo/VistaMapa.java
@@ -11,7 +11,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class VistaMapa extends FragmentActivity {
 
 
-    private int castulo;
+    private int flag;
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
         // Esto es un cambio de mapa de castulo
     @Override
