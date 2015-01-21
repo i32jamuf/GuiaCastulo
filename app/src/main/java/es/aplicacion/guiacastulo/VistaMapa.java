4b0a577a84b2cf27b7785eaf0b2201da3cf2836c
i32jamuf <i32jamuf@hotmail.com>
2015-01-21 12:29:43 +0100

    cambio

--- app/src/main/java/es/aplicacion/guiacastulo/VistaMapa.java
+++ app/src/main/java/es/aplicacion/guiacastulo/VistaMapa.java
@@ -11,7 +11,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class VistaMapa extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-
+// Esto es un cambio de mapa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
